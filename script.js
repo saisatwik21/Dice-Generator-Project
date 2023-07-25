30352e63fed2a728a8833efe0f183ea78a4cbe49
Satwik <97023666+saisatwik21@users.noreply.github.com>
2023-07-25 18:46:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ function roll() {
         let dieOneValue = Math.floor(Math.random() * 6);
         let dieTwoValue = Math.floor(Math.random() * 6);
         console.log(dieOneValue, dieTwoValue);
-        document.querySelector("#die-1").setAttribute("src", "images/" + images[dieOneValue]);
-        document.querySelector("#die-2").setAttribute("src", "images/" + images[dieTwoValue]);
+        document.querySelector("#die-1").setAttribute("src", "/images/" + images[dieOneValue]);
+        document.querySelector("#die-2").setAttribute("src", "/images/" + images[dieTwoValue]);
         document.querySelector("#total").innerHTML = "Your roll is " + ((dieOneValue + 1) + (dieTwoValue + 1));
     },
         1000
